88bbbd69c1b8a15775fdd4ef83f87c115f68b54c
Nickolas <nickolasalcazar@gmail.com>
2023-10-26 16:42:32 -0700

    Resolved CORS error

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 const port = 3000;
 
 import yelpRouter from "./routes/yelp";
+
+// Allow requests from all origins (make this more restrictive in production)
+app.use(cors());
 
 // import gpt from "./services/gpt";
 // gpt.summarize("Hello! Tell me your purpose.");
